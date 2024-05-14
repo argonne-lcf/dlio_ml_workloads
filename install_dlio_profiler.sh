777927d998faa71acc822dcb9ee54841b81fcb1b
Huihuo Zheng <zhenghh04@gmail.com>
2024-05-14 09:46:20 -0500

    Update install_dlio_profiler.sh
    reflect the change of the software environment on Polaris

--- install_dlio_profiler.sh
+++ install_dlio_profiler.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-export HWLOC_PREFIX=$HOME/PolarisAT_eagle/pyenvs/hwloc/
+export HWLOC_PREFIX=/soft/libraries/hwloc/
 export CPATH=$HWLOC_PREFIX/include/:$CPATH
 export LIBRARY_PATH=$HWLOC_PREFIX/lib:$LIBRARY_PATH
 module load cmake
 pip uninstall dlio-profiler-py
-[ -e /tmp/dlio-profiler ] || git clone https://github.com/hariharan-devarajan/dlio-profiler.git
-git clone https://github.com/open-mpi/hwloc.git
-cd hwloc
-./autogen.sh
-./configure --prefix=$HWLOC_PREFIX
-make all install -j4
-cd -
+[ -e /tmp/dlio-profiler ] || git clone https://github.com/hariharan-devarajan/dlio-profiler.git /tmp/dlio-profiler
 cd /tmp/dlio-profiler
 libpath=`which python`
 libpath=${libpath%/bin/python}/lib/
+export CC=gcc-12
+export CXX=g++-12
 git submodule update --init --recursive
 rm -rvf $libpath/libgocha* $libpath/libdlio_profiler* $libpath/libcpp-logger.so $libpath/libbrahma* build *.egg*
 git pull
