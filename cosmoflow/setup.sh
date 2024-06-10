29ec62fc8f61ef37cabf229f11d8d873dcc5f37d
Huihuo Zheng <zhenghh04@gmail.com>
2024-06-10 15:30:55 -0500

    Update setup.sh

--- cosmoflow/setup.sh
+++ cosmoflow/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source /home/hzheng/PolarisAT_eagle/dlio_ml_workloads/setup_ml_env.sh
-BOOST=$WORKDIR/cosmoflow/boost
-export CPATH=${BOOST}/:$CPATH
-export LD_LIBRARY_PATH=${BOOST}/stage/lib:$LD_LIBRARY_PATH
+BOOST_DIR=/eagle/DLIO/soft/boost-1.85.0/
+export CPATH=${BOOST_DIR}/:$CPATH
+export LD_LIBRARY_PATH=${BOOST_DIR}/stage/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/hzheng/PolarisAT_eagle/dlio_ml_workloads/cosmoflow/utils/csrc/build/:$LD_LIBRARY_PATH
 export PYTHONPATH=/home/hzheng/PolarisAT_eagle/dlio_ml_workloads/:$PYTHONPATH
