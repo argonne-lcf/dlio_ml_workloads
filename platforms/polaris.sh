6094a026b02a8aa59c9b2710bb94a1512da3c581
Huihuo Zheng <zhenghh04@gmail.com>
2024-06-22 15:39:29 +0000

    added platforms

---
+++ platforms/polaris.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env
+module use /soft/modulefiles
+module load conda/2024-04-29
+export HWLOC_DIR=/soft/libraries/hwloc/
+
+#NCCL related libarary
+
+export NCCL_CROSS_NIC=1
+export NCCL_COLLNET_ENABLE=1
+export NCCL_NET="AWS Libfabric"
+export LD_LIBRARY_PATH=/soft/libraries/aws-ofi-nccl/v1.9.1-aws/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${HWLOC_DIR}/lib/:$LD_LIBRARY_PATH
+export FI_CXI_DISABLE_HOST_REGISTER=1
+export FI_MR_CACHE_MONITOR=userfaultfd
+export FI_CXI_DEFAULT_CQ_SIZE=131072
+
